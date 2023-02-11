7f1bf2a849529671e0f6455e5ca2dc7356ffc947
masQ-21dev <mqomarudin0480@gmail.com>
2023-02-12 03:38:18 +0700

    tampilan 90% beres tanpa bug

--- app/Views/capresma.php
+++ app/Views/capresma.php
@@ -6,8 +6,6 @@
     <title>PEMIRA 2022</title>
     <meta name="description" content="The small framework with powerful features">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="shortcut icon" type="image/png" href="/favicon.ico" />
-    <link rel="stylesheet" href="<?php echo base_url('/css/app.css') ?>">
     <?php
                 foreach($detail_capresma as $details) {
                     
@@ -16,6 +14,8 @@
     <?php
             }
             ?>
+    <link rel="shortcut icon" type="image/png" href="/favicon.ico" />
+    <link rel="stylesheet" href="<?php echo base_url('/css/app.css') ?>">
 
     <script src="https://kit.fontawesome.com/861b96d111.js" crossorigin="anonymous"></script>
 </head>
